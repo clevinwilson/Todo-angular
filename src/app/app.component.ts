aad881c40312ea401a1c8676dabcfc18a3b7d472
Clevin M Wilson <clevinmwilson@Clevins-MacBook-Pro.local>
2024-11-24 10:03:09 +0530

    todo list final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,87 +1,26 @@
-import {
-  Component,
-  OnInit,
-  signal,
-  WritableSignal,
-} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Component } from '@angular/core';
+
 import { HeaderComponent } from './header/header.component';
-import { UserCardComponent } from './user-card/user-card.component';
-import { TaskCardComponent } from './task-card/task-card.component';
-import { TaskService } from './services/task.service';
-import { MatDialog } from '@angular/material/dialog';
-import { AddTaskComponent } from './add-task/add-task.component';
-import { task } from './interface/interface';
-import { CommonModule } from '@angular/common'; 
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    MatSlideToggleModule,
-    HeaderComponent,
-    UserCardComponent,
-    TaskCardComponent,
-    CommonModule
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent, UserComponent, TasksComponent]
 })
-export class AppComponent implements OnInit {
-  constructor(private taskService: TaskService, public dialog: MatDialog) {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectedUserId?: string;
 
-  users: WritableSignal<{ id: number; name: string; photo: string }[]> = signal(
-    []
-  );
-  tasks: WritableSignal<task[]> = signal([]);
-  title = 'todo-list';
-
-  ngOnInit(): void {
-    this.users.set(this.taskService.users);
-    this.tasks.set(this.taskService.task);
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
-  /**
-   * Add new task.
-   * @param task
-   * @returns void.
-   */
-  addNewTask(task: task): void {
-    this.tasks.update((currentTasks) => [...currentTasks, task]);
-  }
-
-  /**
-   * Open add task dialog box
-   * @returns void.
-   */
-  openAddTaskDialog(): void {
-    const dialogRef = this.dialog.open(AddTaskComponent, {
-      width: '600px',
-    });
-
-    dialogRef.afterClosed().subscribe((result) => {
-      if (result) {
-        this.addNewTask({
-          ...result,
-          id: Math.random(),
-          userId: 1,
-          createDate: Date.now(),
-        });
-      }
-    });
-  }
-
-  markAsCompleted(id:number){
-    console.log(id,'event');
-    console.log(this.tasks());
-    const updatedTaskList = this.tasks().map((task)=>{
-      if(task.id === id){
-        return { ...task, status:'COMPLETED'}
-      } 
-      return task;
-    });
-    this.tasks.update(() => [...updatedTaskList])
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
   }
 }
