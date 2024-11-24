8907f361dc9a802cb32f7eb550aaca8dc5c58c10
Clevin M Wilson <clevinmwilson@Clevins-MacBook-Pro.local>
2024-11-24 16:53:45 +0530

    all standalone components moved to module based component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule } from "@angular/core";
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { UserComponent } from "./user/user.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { BrowserModule } from "@angular/platform-browser";
-import { TasksComponent } from "./tasks/tasks.component";
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    declarations:[AppComponent],
-    bootstrap:[AppComponent],
-    imports: [BrowserModule, HeaderComponent, UserComponent, TaskComponent, TasksComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TaskComponent,
+    TasksComponent,
+    NewTaskComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [BrowserModule, FormsModule],
 })
-
-export class AppModule {
-
-}
+export class AppModule {}
